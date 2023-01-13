62cc2bb668416d6a0404fa55b7cecb0235f16f11
MacioO-69 <63945390+MacioO-69@users.noreply.github.com>
2023-01-13 12:29:53 +0100

    skript

--- hello.js
+++ hello.js
@@ -1,26 +1,26 @@
 let a = 1;
-function dodaj(){
+function dodaj() {
     let inx = a.toString();
     let element = document.createElement("tr");
-    let text = (<HTMLInputElement>document.getElementById("d1")).value;
+    let text = document.getElementById("d1").value;
     element.id = inx;
-    let nr = " [nr "+ inx+ "]";
+    let nr = " [nr " + inx + "]";
     element.textContent = text + nr;
     element.className = "ad";
     document.getElementById("t1").append(element);
-    a = a + 1
+    a = a + 1;
 }
-function zmien(){
-    let idn = (<HTMLInputElement>document.getElementById("d3")).value;
-    let text2 = (<HTMLInputElement>document.getElementById("d2")).value;
-    let iden = " [nr " + idn +"]";
+function zmien() {
+    let idn = document.getElementById("d3").value;
+    let text2 = document.getElementById("d2").value;
+    let iden = " [nr " + idn + "]";
     let text = text2 + iden;
     console.log(text);
     document.getElementById(idn).textContent = text;
 }
-function usun(){
+function usun() {
     let parent = document.getElementById("t1");
-    let idd = (<HTMLInputElement>document.getElementById("d4")).value;
+    let idd = document.getElementById("d4").value;
     let child = document.getElementById(idd);
     parent.removeChild(child);
 }
